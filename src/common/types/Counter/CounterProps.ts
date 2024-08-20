56ed426330f8d649205a3f4eb50b410a59e273f6
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-21 02:10:21 +0300

    Add new props to CounterProps
    Added 'message' and 'isActive' properties to the CounterProps type. These new properties will help in managing the state and display of the counter component more effectively.

--- src/common/types/Counter/CounterProps.ts
+++ src/common/types/Counter/CounterProps.ts
@@ -2,6 +2,8 @@ export type CounterProps = {
     startValue: number;
     maxValue: number;
     counter: number
+    message: string | null
     error: string | null
+    isActive: boolean
     setCounter?: (counter: number) => void
 }
