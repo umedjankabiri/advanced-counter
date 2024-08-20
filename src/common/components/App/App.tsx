736d9332fad3b13ce1b89c5a413b9f2358c6687b
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-20 09:29:41 +0300

    added components, and created all logical functionality

--- src/common/components/App/App.tsx
+++ src/common/components/App/App.tsx
@@ -1,11 +1,67 @@
 import 'components/App/App.css'
+import {Counter} from "components/Counter/Counter.tsx";
+import {ChangeEvent, useState} from "react";
+import {Settings} from "components/Settings.tsx";
 
 function App() {
-  return (
-    <div  className={"App"}>
-      <h1>Advanced counter</h1>
-    </div>
-  )
+    const zeroValue = 0;
+    const minusOne = -1;
+
+    const [maxValue, setMaxValue] = useState<number>(zeroValue)
+    const [startValue, setStartValue] = useState<number>(zeroValue)
+    const [error, setError] = useState<string | null>(null)
+
+    let setCounterFunction: ((counter: number) => void) | null = null;
+
+    const onChangeMaxValueHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const tempMaxValue = +event.currentTarget.value
+        if (tempMaxValue == minusOne)
+            setError(`max value cannot be less then ${zeroValue}`)
+        else
+            setError(null)
+        setMaxValue(tempMaxValue)
+    }
+    const onChangeStartValueHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const tempStartValue = +event.currentTarget.value
+        if (tempStartValue == minusOne)
+            setError(`start value cannot be less then ${zeroValue}`)
+        else if (tempStartValue == maxValue)
+            setError(`start value cannot be equal to maxValue`)
+        else if (tempStartValue > maxValue)
+            setError(`start value cannot be biggest then ${maxValue}`)
+        else
+            setError(null)
+        setStartValue(tempStartValue)
+    }
+    const enterValues = () => {
+        setCounterFunction && setCounterFunction(startValue)
+    }
+    const enterStartValue = (setCounter: (counter: number) => void) => {
+        setCounterFunction = setCounter
+    }
+    const clearValues = () => {
+        setError(null)
+        setStartValue(zeroValue)
+        setMaxValue(zeroValue)
+    }
+
+    return (
+        <div className={"mainWrapper"}>
+            <Settings maxValue={maxValue}
+                      startValue={startValue}
+                      onChangeStart={onChangeStartValueHandler}
+                      onChangeMax={onChangeMaxValueHandler}
+                      onClickEnter={enterValues}
+                      onClickClear={clearValues}
+                      error={error}
+            />
+            <Counter maxValue={maxValue}
+                     startValue={startValue}
+                     error={error}
+                     onClickEnter={enterStartValue}
+            />
+        </div>
+    )
 }
 
 export default App
